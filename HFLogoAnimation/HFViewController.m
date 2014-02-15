0afcf7e6ede74b4ba9eeb98cc7cbcd2133e25bda
Gregory Klein <gregoryhklein@gmail.com>
2014-02-15 02:38:17 -0500

    Changed the way "HardFlip" text layers are set up.

--- HFLogoAnimation/HFViewController.m
+++ HFLogoAnimation/HFViewController.m
@@ -10,7 +10,7 @@
 #import "HFTextLayer.h"
 
 @interface HFViewController ()
-@property (strong, nonatomic) HFTextLayer *hLayer;
+@property (strong, nonatomic) NSArray *hardFlipLetters;
 @end
 
 @implementation HFViewController
@@ -18,33 +18,42 @@
 - (void)viewDidLoad
 {
    [super viewDidLoad];
+   self.hardFlipLetters = @[@"H", @"a", @"r", @"d", @"F", @"l", @"i", @"p"];
    [self setupTextLayers];
-}
-
-- (void)didReceiveMemoryWarning
-{
-   [super didReceiveMemoryWarning];
-   // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Helper Methods
 - (void)setupTextLayers
 {
-   self.hLayer = [HFTextLayer textLayerWithLetter:@"H"];
-   self.hLayer.bounds = CGRectMake(0, 0, 50, 50);
-   self.hLayer.position = CGPointMake(CGRectGetMidX(self.view.bounds),
-                                CGRectGetMidY(self.view.bounds));
-   
-   self.hLayer.backgroundColor = [UIColor colorWithRed:.9 green:0 blue:.8 alpha:.5].CGColor;
+   CALayer *letterContainer = [CALayer layer];
+   CGPoint previousPosition = CGPointZero;
+   for (NSString *letter in self.hardFlipLetters)
+   {
+      CALayer *letterLayer = [HFTextLayer textLayerWithLetter:letter];
+      letterLayer.bounds = CGRectMake(0, 0, 50, 50);
+      letterLayer.position = previousPosition;
+      letterLayer.anchorPoint = CGPointZero;
+      letterLayer.name = letter;
+      letterContainer.bounds = CGRectMake(0, 0, CGRectGetWidth(letterContainer.bounds) +
+                                          CGRectGetWidth(letterLayer.bounds),
+                                          CGRectGetHeight(letterLayer.bounds));
 
-   [self.view.layer addSublayer:self.hLayer];
-   [self.hLayer setNeedsDisplay];
+      [letterContainer addSublayer:letterLayer];
+      [letterLayer setNeedsDisplay];
+
+      previousPosition = CGPointMake(letterLayer.position.x + CGRectGetWidth(letterLayer.bounds),
+                                     letterLayer.position.y);
+   }
+
+   letterContainer.backgroundColor = [UIColor colorWithRed:1 green:0 blue:.2 alpha:.5].CGColor;
+   letterContainer.position = CGPointMake(CGRectGetMidY(self.view.frame),
+                                          CGRectGetMidX(self.view.frame));
+   
+   [self.view.layer addSublayer:letterContainer];
 }
 
 - (IBAction)animate:(id)sender
 {
-   self.hLayer.position = CGPointMake(self.hLayer.position.x + 10,
-                                      self.hLayer.position.y);
 }
 
 @end
